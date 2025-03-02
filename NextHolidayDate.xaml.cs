892178f1e0459879bfd93c15f1b7e3d35c6840cf
jiangyin14 <1219439938@qq.com>
2025-03-02 20:59:12 +0800

    添加NextHolidayDate组件及类

--- NextHolidayDate.xaml.cs
+++ NextHolidayDate.xaml.cs
@@ -1,6 +1,25 @@
-﻿namespace TodayIsland;
+﻿using ClassIsland.Core.Abstractions.Controls;
+using ClassIsland.Core.Attributes;
+using MaterialDesignThemes.Wpf;
 
-public class NextHolidayDate_xaml
+namespace TodayIsland;
+
+[ComponentInfo(
+    "6DD56598-8C6E-C22D-D642-053EC1B0A1F1",
+    "日月起落时间",
+    PackIconKind.CalendarOutline,
+    "在主界面显示本周在学期中的周数。"
+)]
+public partial class NextHolidayDateControl : ComponentBase
 {
-    
+    public NextHolidayDateControl()
+    {
+        InitializeComponent();
+        LoadRiseFallTimeAsync();
+    }
+
+    private async void LoadRiseFallTimeAsync()
+    {
+        
+    }
 }
